33b38993e63248c52dffd2db329c9c4bc689b67f
franklyn <Androidtecknowlogy@gmai.com>
2016-10-29 09:12:26 +0100

    Slash activity and product xml working progress

--- app/src/main/java/com/kongahack/negotiator/activity/ProductActivity.java
+++ app/src/main/java/com/kongahack/negotiator/activity/ProductActivity.java
@@ -1,8 +1,17 @@
 package com.kongahack.negotiator.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by nezspencer on 10/29/16.
  */
 
-public class ProductActivity {
+public class ProductActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
--- app/src/main/java/com/kongahack/negotiator/activity/SplashActivity.java
+++ app/src/main/java/com/kongahack/negotiator/activity/SplashActivity.java
@@ -1,8 +1,60 @@
 package com.kongahack.negotiator.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.kongahack.negotiator.R;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
 /**
  * Created by nezspencer on 10/29/16.
  */
 
-public class SplashActivity {
+public class SplashActivity extends AwesomeSplash{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void animationsFinished() {
+        startActivity(new Intent(SplashActivity.this, ProductActivity.class));
+        SplashActivity.this.finish();
+
+
+    }
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        overridePendingTransition(
+                R.anim.activity_open_translate, R.anim.activity_close_scale);
+
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.home); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(700); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_TOP); //or Flags.REVEAL_TOP
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.smack);
+        configSplash.setAnimLogoSplashDuration(500); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn);
+
+        configSplash.setTitleSplash("smacK");
+        configSplash.setTitleTextColor(android.R.color.black);
+        configSplash.setTitleTextSize(48f); //float value
+        configSplash.setAnimTitleDuration(600);
+        configSplash.setAnimTitleTechnique(Techniques.Flash);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 }
